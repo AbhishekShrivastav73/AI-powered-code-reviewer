c29871dcfad81abfa6d583cbf3986bff9bef1f1d
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2025-02-13 10:28:11 +0530

    Code UI fixes

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="w-full bg-zinc-950 h-16 flex px-6 py-2 items-center justify-between">
-      <div className="text-[#E6EDF3] tracking-tight font-bold text-md">
+    <div className="w-full bg-[#0D1117] h-16 flex px-6 py-2 items-center justify-between">
+      <Link to={'/'} className="text-[#E6EDF3] tracking-tight font-bold text-md">
         AI-Powered Code Reviewer
-      </div>
+      </Link>
       <div className="flex items-center gap-4">
-        <Link to={'/'} className="text-[#E6EDF3] mr-2 tracking-tighter font-semibold">Home</Link>
-        <Link className="bg-[#0095ff] hidden md:block text-[#E6EDF3]  px-6 py-2 rounded-full font-bold ">
+        
+        <Link to={'https://github.com/AbhishekShrivastav73'} className="bg-[#0095ff] hidden md:block text-[#E6EDF3]  px-6 py-1 rounded-full font-bold ">
           <span className="mr-2">GitHub</span>
-          <i className="text-lg text-black" class="ri-github-fill"></i>
+          <i className="text-lg text-white ri-github-fill"></i>
         </Link>
       </div>
     </div>
--- frontend/src/pages/CodeAI.jsx
+++ frontend/src/pages/CodeAI.jsx
@@ -3,28 +3,64 @@ import Navbar from "../Components/Navbar";
 function CodeAI() {
   return (
     <>
-    <Navbar/>
-    <div className="w-full min-h-screen text-zinc-50 bg-[#0D1117]">
-      <div className="top w-full flex flex-col justify-center h-[80vh] items-center">
-        <h1 className="text-7xl text-[#8B949E] text-center font-bold tracking-tighter">
-          Enter Your <b className="relative text-[#E6EDF3]"> <img src="https://imgs.search.brave.com/hS8LtKrYPRqMPAn-zpkObgZkPKXoiofr5aNza0_siNs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYWR2ZXJ0aXNp/bmctbWVkaWEtMS82/NC9BZHZlcnRpc2lu/Z19fTWVkaWFfSWNv/bnMtMTAtMTI4LnBu/Zw" className="w-12 absolute -top-4 -right-6 rounded-xl" alt="" /> Code</b> <br /> For <b className="text-[#E6EDF3]">Review</b>.
-        </h1>
-        <p className="text-sm text-center text-[#8B949E] mt-2 tracking-tight">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam
-          nostrum eligendi explicabo, <br /> maiores dicta cupiditate ea
-          veritatis. Ut dolorem quae quo optio.
-        </p>
-        <div className="bg-[#161B22] w-[70%] rounded-full mt-12 relative p-2">
-          <button className="bg-[#0095ff] tracking-tight font-bold text-[#E6EDF3] px-4 py-2 rounded-full hover:bg-[#1F6FEB] absolute top-2 right-2">Review Code</button>
+      <Navbar />
+      <div className="w-full min-h-screen text-zinc-50 bg-[#0D1117]">
+        <div className="top w-full flex flex-col justify-center h-[80vh] items-center">
+          <h1 className="text-5xl text-[#8B949E] text-center font-bold tracking-tighter">
+            Your{" "}
+            <b className="relative  text-[#0095ff]">
+              {" "}
+              AI - Code {" "}
+              <img
+                src="https://imgs.search.brave.com/hS8LtKrYPRqMPAn-zpkObgZkPKXoiofr5aNza0_siNs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYWR2ZXJ0aXNp/bmctbWVkaWEtMS82/NC9BZHZlcnRpc2lu/Z19fTWVkaWFfSWNv/bnMtMTAtMTI4LnBu/Zw"
+                className="w-12 absolute -top-4 -right-6 rounded-xl md:hidden"
+                alt=""
+              />
+            </b>{" "}
+            <b className="text-[#E6EDF3] relative">
+            Reviewer{" "}
+              <img
+                src="https://imgs.search.brave.com/hS8LtKrYPRqMPAn-zpkObgZkPKXoiofr5aNza0_siNs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYWR2ZXJ0aXNp/bmctbWVkaWEtMS82/NC9BZHZlcnRpc2lu/Z19fTWVkaWFfSWNv/bnMtMTAtMTI4LnBu/Zw"
+                className="w-12 absolute -top-6 -right-6 hidden md:block rounded-xl"
+                alt=""
+              />
+            </b>
+            .
+          </h1>
+          <div>
+            <h2 className="text-6xl mt-3 mb-2 text-[#8B949E] text-center font-black tracking-tighter">
+              <span className="text-[#E6EDF3]">SMART</span>,
+              <span className="text-[#0095ff]">FAST</span>,
+              <span className="text-[#E6EDF3]">RELIABLE</span>
+            </h2>
+          </div>
+          <p className="text-sm text-center text-[#8B949E] mt-2 tracking-tight">
+            This AI-powered code reviewer helps you review your code for <br />{" "}
+            syntax errors, logical errors, and security vulnerabilities.
+          </p>
 
-          <input
-            className="text-white outline-none px-4 py-2 rounded-full w-full "
-            name="code"
-            placeholder="Enter your code here.."
-          />
+          <div className="bg-[#161B22] w-[70%] rounded-2xl mt-12 relative p-2">
+            <button className="bg-[#0095ff] tracking-tight font-bold text-[#E6EDF3] px-4 py-2 rounded-lg hover:bg-[#1F6FEB] absolute bottom-3 right-2">
+              Review Code
+            </button>
+
+            <textarea
+              className="text-white outline-none px-4 py-2 rounded-xl w-full resize-none"
+              name="code"
+              placeholder="Enter your code here.."
+              style={{
+                height: "60px",
+                maxHeight: "200px",
+                overflowY: "scroll",
+              }}
+              onChange={(e) => {
+                e.target.style.height = "inherit";
+                e.target.style.height = `${e.target.scrollHeight}px`;
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
