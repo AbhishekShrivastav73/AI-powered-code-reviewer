087eed7c1cd938e853ecbf595cd5a44184fd2dfb
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2025-02-13 11:45:03 +0530

    made responsive and add external links to btns

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@
 import { Route, Routes } from 'react-router-dom';
 import CodeAI from './pages/CodeAI';
 import Code from './pages/Code';
+import Home from './pages/Home';
 
 function App() {
   return (
    <>
     <Routes>
-      <Route path='/' element={<Code/>}/>
-      <Route path='/2' element={<CodeAI/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/1' element={<Code/>}/>
+      <Route path='/code-review' element={<CodeAI/>}/>
     </Routes>
    </>
   )
--- frontend/src/pages/CodeAI.jsx
+++ frontend/src/pages/CodeAI.jsx
@@ -5,15 +5,15 @@ function CodeAI() {
     <>
       <Navbar />
       <div className="w-full min-h-screen text-zinc-50 bg-[#0D1117]">
-        <div className="top w-full flex flex-col justify-center h-[80vh] items-center">
-          <h1 className="text-5xl text-[#8B949E] text-center font-bold tracking-tighter">
+        <div className="top w-full flex flex-col justify-center h-[87vh] items-center">
+          <h1 className="text-3xl md:text-5xl text-[#8B949E] text-center font-bold tracking-tighter">
             Your{" "}
             <b className="relative  text-[#0095ff]">
               {" "}
               AI - Code {" "}
               <img
                 src="https://imgs.search.brave.com/hS8LtKrYPRqMPAn-zpkObgZkPKXoiofr5aNza0_siNs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG40/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvYWR2ZXJ0aXNp/bmctbWVkaWEtMS82/NC9BZHZlcnRpc2lu/Z19fTWVkaWFfSWNv/bnMtMTAtMTI4LnBu/Zw"
-                className="w-12 absolute -top-4 -right-6 rounded-xl md:hidden"
+                className="w-12 absolute -top-10 -right-32 rounded-xl md:hidden"
                 alt=""
               />
             </b>{" "}
@@ -28,20 +28,20 @@ function CodeAI() {
             .
           </h1>
           <div>
-            <h2 className="text-6xl mt-3 mb-2 text-[#8B949E] text-center font-black tracking-tighter">
+            <h2 className="text-5xl  flex flex-wrap items-center justify-center md:text-7xl mt-3 mb-2 text-[#8B949E] text-center font-black tracking-tighter">
               <span className="text-[#E6EDF3]">SMART</span>,
               <span className="text-[#0095ff]">FAST</span>,
               <span className="text-[#E6EDF3]">RELIABLE</span>
             </h2>
           </div>
-          <p className="text-sm text-center text-[#8B949E] mt-2 tracking-tight">
-            This AI-powered code reviewer helps you review your code for <br />{" "}
+          <p className="text-xs md:text-sm text-center px-5 text-[#8B949E] mt-2 tracking-tight">
+            This AI-powered code reviewer helps you review your code for <br className="hidden md:block" /> {" "}
             syntax errors, logical errors, and security vulnerabilities.
           </p>
 
-          <div className="bg-[#161B22] w-[70%] rounded-2xl mt-12 relative p-2">
+          <div className="bg-[#161B22] w-[85%] md:w-[70%] rounded-xl mt-12 relative p-2">
             <button className="bg-[#0095ff] tracking-tight font-bold text-[#E6EDF3] px-4 py-2 rounded-lg hover:bg-[#1F6FEB] absolute bottom-3 right-2">
-              Review Code
+            <i className="ri-arrow-up-line"></i>
             </button>
 
             <textarea
@@ -59,6 +59,7 @@ function CodeAI() {
               }}
             />
           </div>
+             <p className="text-sm text-center text-[#8B949E] mt-4">Powered by Gemini <i className="ri-gemini-fill"></i></p>
         </div>
       </div>
     </>
