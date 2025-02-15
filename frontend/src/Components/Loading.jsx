9406428fb78393021eca7d5911287aa17227aebd
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2025-02-15 15:25:37 +0530

    fixed loader

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -1,39 +1,20 @@
 function Loading() {
   return (
-    <>
-    <div role="status" className="max-w-md animate-pulse">
-      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[45%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[49%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%]"></div>
-      <span className="sr-only">Loading...</span>
-    </div>
-    <div role="status" className="max-w-md animate-pulse">
-      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[45%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[49%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%]"></div>
-      <span className="sr-only">Loading...</span>
-    </div>
-    <div role="status" className="max-w-md animate-pulse">
-      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[45%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[49%] mb-2.5"></div>
-      <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[60%]"></div>
-      <span className="sr-only">Loading...</span>
-    </div>
-    </>
+      <>
+          {Array(3).fill(null).map((_, i) => (
+              <div role="status" className="max-w-md *:bg-gray-200 *:dark:bg-gray-700 *:rounded-full animate-pulse mb-4" key={i}>
+                  <div className="h-2.5 w-48 mb-4"></div>
+                  <div className="h-2 max-w-[60%] mb-2.5"></div>
+                  <div className="h-2 max-w-[45%] mb-2.5"></div>
+                  <div className="h-2 mb-2.5"></div>
+                  <div className="h-2 mb-2.5"></div>
+                  <div className="h-2 max-w-[49%] mb-2.5"></div>
+                  <div className="h-2 max-w-[60%]"></div>
+                  <span className="sr-only">Loading...</span>
+              </div>
+          ))}
+      </>
   );
 }
 
 export default Loading;
-
